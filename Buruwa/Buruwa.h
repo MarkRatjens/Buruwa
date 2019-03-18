6eec02634aa6acb3afa5e4d2192003ebd06e49d1
Mark Ratjens <mark@ratjens.com>
2019-03-18 14:30:52 +1100

    Initial Commit

---
+++ Buruwa/Buruwa.h
@@ -0,0 +1,19 @@
+//
+//  Buruwa.h
+//  Buruwa
+//
+//  Created by Mark Ratjens on 18/3/19.
+//  Copyright © 2019 Nyasa Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Buruwa.
+FOUNDATION_EXPORT double BuruwaVersionNumber;
+
+//! Project version string for Buruwa.
+FOUNDATION_EXPORT const unsigned char BuruwaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Buruwa/PublicHeader.h>
+
+
