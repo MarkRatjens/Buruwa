29f2cdd047b326d7740d22f0ebd54d3f47a4c27b
Mark Ratjens <mark@ratjens.com>
2019-05-21 11:08:36 +1000

    implement get functions for SafeCourier; rename carry to post

--- Buruwa/Buruwa.h
+++ Buruwa/Buruwa.h
@@ -3,7 +3,6 @@
 //  Buruwa
 //
 //  Created by Mark Ratjens on 18/3/19.
-//  Copyright © 2019 Nyasa Corp. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- Buruwa/Courier.swift
+++ Buruwa/Courier.swift
@@ -2,29 +2,31 @@ import Foundation
 import UIKit
 
 extension REST {
-	public class Courier<D: Decodable> {
-		open func carry(parcel: Parcel<D>, for request: URLRequest, then complete: @escaping ([D]?, URLResponse?, Error?) -> Void) {
-			let c = URLSessionConfiguration.ephemeral
-			let s = URLSession(configuration: c, delegate: nil, delegateQueue: OperationQueue.main)
-			let t = s.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
-				if error != nil { print("NyasaKit.REST Error: \(error!.localizedDescription)") }
-				
-				guard let d = data else {
-					print("NyasaKit.REST Error: did not receive data")
-					complete(nil, response, error)
-					return
-				}
-				
-				let r = parcel.resources(from: d)
-				complete(r, response, error)
-			})
-			t.resume()
-		}
-	}
-
-	
 	public class SafeCourier<D: Decodable>: Courier<D> {
-		public func carry(parcel: Parcel<D>, on path: String, with parameters: Serialization, then complete: @escaping ([D]?, URLResponse?, Error?) -> Void) {
+		public func get(parcel: Parcel<D>, on path: String, with parameters: Serialization, then complete: @escaping ([D]) -> Void) {
+			get(parcel: parcel, on: path, with: parameters, then: { (result: [D]?, response: URLResponse?, error: Error?) in
+				if let r = result { complete(r) }
+			})
+		}
+		
+		public func post(parcel: Parcel<D>, on path: String, with parameters: Serialization, then complete: @escaping ([D]) -> Void) {
+			post(parcel: parcel, on: path, with: parameters, then: { (result: [D]?, response: URLResponse?, error: Error?) in
+				if let r = result { complete(r) }
+			})
+		}
+
+		private func get(parcel: Parcel<D>, on path: String, with parameters: Serialization, then complete: @escaping ([D]?, URLResponse?, Error?) -> Void) {
+			let s = path + "?" + parameters.asHttpQuery
+			var r = URLRequest(url: parcel.path(s))
+			r.httpMethod = "GET"
+			
+			r.addValue("application/json", forHTTPHeaderField: "Content-Type")
+			r.addValue("application/json", forHTTPHeaderField: "Accept")
+			
+			carry(parcel: parcel, for: r, then: complete)
+		}
+
+		private func post(parcel: Parcel<D>, on path: String, with parameters: Serialization, then complete: @escaping ([D]?, URLResponse?, Error?) -> Void) {
 			var r = URLRequest(url: parcel.path(path))
 			r.httpMethod = "POST"
 			
@@ -35,15 +37,40 @@ extension REST {
 			catch let error { print(error.localizedDescription) }
 			carry(parcel: parcel, for: r, then: complete)
 		}
-	
-		public func carry(parcel: Parcel<D>, on path: String, with parameters: Serialization, then complete: @escaping ([D]) -> Void) {
-			carry(parcel: parcel, on: path, with: parameters, then: { (result: [D]?, response: URLResponse?, error: Error?) in
-				if let r = result { complete(r) }
-			})
-		}
 		
 		override public init() {}
 	}
+
+	public class Courier<D: Decodable> {
+		func carry(parcel: Parcel<D>, for request: URLRequest, then complete: @escaping ([D]?, URLResponse?, Error?) -> Void) {
+			let c = URLSessionConfiguration.ephemeral
+			let s = URLSession(configuration: c, delegate: nil, delegateQueue: OperationQueue.main)
+			let t = s.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+				if error != nil { print("Buruwa.REST Error: \(error!.localizedDescription)") }
+				
+				guard let d = data else {
+					print("Buruwa.REST Error: did not receive data")
+					complete(nil, response, error)
+					return
+				}
+				
+				let r = parcel.resources(from: d)
+				complete(r, response, error)
+			})
+			t.resume()
+		}
+	}
 }
 
+
 public typealias Serialization = [String: Any]
+
+extension Serialization {
+	var asHttpQuery: String {
+		return map {
+			let p = $0.key.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+			let v = (String(describing: $0.value)).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+			return "\(p)=\(v)"
+			}.joined(separator: "&")
+	}
+}
